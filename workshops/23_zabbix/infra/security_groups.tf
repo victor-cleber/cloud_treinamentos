f69e3fa9294912c5779bb8030f1d92fe98bb5892
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2025-01-24 22:41:28 +1300

    EC2 created

---
+++ workshops/23_zabbix/infra/security_groups.tf
@@ -0,0 +1,27 @@
+# Instance Security group (traffic ALB -> EC2, ssh -> EC2)
+resource "aws_security_group" "ec2" {
+  name        = "ec2_security_group"
+  description = "Allows inbound access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    #security_groups = [aws_security_group.load-balancer.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- workshops/23_zabbix/infra/variables.tf
+++ workshops/23_zabbix/infra/variables.tf
@@ -45,3 +45,10 @@ variable "course" {
   type        = string
   default     = "Cloud Training - AWS bootcamp"
 }
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
