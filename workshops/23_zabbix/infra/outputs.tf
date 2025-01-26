a4c1598df3a2da1e291f9ba5d69a88de0e0e73e4
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2025-01-26 20:53:22 +1300

    Added RDS

--- workshops/23_zabbix/infra/outputs.tf
+++ workshops/23_zabbix/infra/outputs.tf
@@ -2,9 +2,9 @@ output "vpc_arn" {
   value = aws_vpc.main.arn
 }
 
-output "rds_endpoint" {
-  value = aws_db_instance.db_instance.endpoint
-}
+# output "rds_endpoint" {
+#   value = aws_db_instance.db_instance.endpoint
+# }
 
 output "rds_password" {
   value     = aws_db_instance.db_instance.password
